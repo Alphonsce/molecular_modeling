15681216f77cf8b9e8123bada465caf4d9bd949d
Alphonsce <varlamov.al@phystech.edu>
2022-02-12 17:31:36 +0300

    started writing code

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-# Minimal parts : <=100
+import numpy as np
+import matplotlib.pyplot as plt
 
 SIGMA = 1
 EPSILON = 1
 M = 1
 
-TIME_STEPS = 500
+TIME_STEPS = 100
 N = 20
 L = N ** (1 / 3)
 
@@ -13,30 +14,18 @@ dt = 0.001
 #T = 
 
 class Particle:
-    def __init__(self, x, y, z, vx, vy, vz):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.vx = vx
-        self.vy = vy
-        self.vz = vz
-        self.fx = 0
-        self.fy = 0
-        self.fz = 0
+    def __init__(self, pos, vel):
+        self.pos = pos
+        self.vel = vel
+        self.acc = 0
 
     def move(self):
-        self.x += self.vx * dt
-        self.y += self.vy * dt
-        self.z += self.vz * dt
-
-        self.vx += (self.fx / M) * dt
-        self.vy += (self.fy / M) * dt
-        self.vz += (self.fz / M) * dt
+        pass
 
 # def initialize_particles():
 #     for _ in range(N):
 
-def main_function(n_particles):
+def main_function():
     '''это главный цикл, в котором будет происходить расчет взаимодействия частиц и их движение
     '''
     pass
