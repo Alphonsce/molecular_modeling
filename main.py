fbdc15938f8a258373015f40106e5b0089990926
Alphonsce <varlamov.al@phystech.edu>
2022-02-13 01:06:31 +0300

    todos added

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ class Particle:
         self.vel = vel
         self.acc = acc
 
-        self.energy = 0.5 * norm(vel) ** 2
+        self.kin_energy = 0     # 0.5 * M * norm(vel) ** 2
+        self.pot_energy = 0
 
     def move(self):
         pass
@@ -55,7 +56,7 @@ def sgn(x):
     return 0
 
 def calculate_acceleration(part1, part2):
-    r = part1.pos - part2.pos       # delta r -
+    r = part1.pos - part2.pos       # r_1 - r_2
     # Boundary condition realisation:
     for i in range(3):
         if abs(r[i]) > L / 2:
@@ -65,6 +66,7 @@ def calculate_acceleration(part1, part2):
     if dist < r_cut:
         part1.acc += force(r) / M       # we add the force from only one particle acting on another to the total acc
         part2.acc += -part1.acc
+        #TODO: add potential energy from interacting with this particle
 
 def main_function():
     '''
@@ -75,6 +77,9 @@ def main_function():
     for ts in range(TIME_STEPS):
         for i in particles:
             i.acc = np.zeros(3)
+            i.kin_energy = 0
+            i.pot_energy = 0
+        #TODO:
         # then calculate new a by adding force from all the particles
         # then move all the particles
         pass
